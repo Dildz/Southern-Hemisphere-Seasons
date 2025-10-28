3e65b47aa92a973e8e067ef1f5a0480514e403a2
Dildz <roy.madejski.jackson@gmail.com>
2025-10-28 13:34:08 +0200

    update SptVersion to ~4.0.2

--- SouthernHemisphereSeasons.cs
+++ SouthernHemisphereSeasons.cs
@@ -20,7 +20,7 @@ public record ModMetadata : AbstractModMetadata{
     public override string Author { get; init; } = "Dildz";
     public override List<string>? Contributors { get; init; }
     public override SemanticVersioning.Version Version { get; init; } = new("2.0.0");
-    public override SemanticVersioning.Range SptVersion { get; init; } = new("~4.0.0");
+    public override SemanticVersioning.Range SptVersion { get; init; } = new("~4.0.2");
     public override List<string>? Incompatibilities { get; init; }
     public override Dictionary<string, SemanticVersioning.Range>? ModDependencies { get; init; }
     public override string? Url { get; init; }
